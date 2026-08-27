28c17b449f0965313de16cc8b98b755273893416
zagaljj <zagaljj@gmail.com>
2026-08-27 18:38:58 -0400

    fix: rewrite /adminlanding to static /adminlanding/index.html in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/adminlanding',
+        destination: '/adminlanding/index.html',
+      },
+      {
+        source: '/adminlanding/',
+        destination: '/adminlanding/index.html',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
