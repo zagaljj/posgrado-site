3e838f74c8c6393cf8ace0c92cb9c59dfcb35999
zagaljj <zagaljj@gmail.com>
2026-08-27 18:12:48 -0400

    feat: add rewrites to proxy landings from LandingBase

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      // Rewrite /diplomados/:slug to Landing Base
+      {
+        source: '/diplomados/:slug',
+        destination: 'https://udi-landing-admin.vercel.app/:slug',
+      },
+      {
+        source: '/diplomados/:slug/assets/:path*',
+        destination: 'https://udi-landing-admin.vercel.app/:slug/assets/:path*',
+      },
+      // Rewrite direct slug e.g. /fullstack
+      {
+        source: '/fullstack',
+        destination: 'https://udi-landing-admin.vercel.app/fullstack',
+      },
+      {
+        source: '/fullstack/assets/:path*',
+        destination: 'https://udi-landing-admin.vercel.app/fullstack/assets/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
